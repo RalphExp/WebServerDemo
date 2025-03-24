7175c5e68f1ce6a591cbe6baeb0cabe9b13b4772
Ralph <ralphxwz@gmail.com>
2025-03-24 16:17:13 +0900

    1

--- WebRTC/main.go
+++ WebRTC/main.go
@@ -4,12 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func handleRoot(c *gin.Context) {
+	c.File("./static/index.html")
+}
+
+func handleWebRTC(c *gin.Context) {
+	param := c.Param("path")
+	c.File("./static/" + param + ".html")
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) { c.File("./static/index.html") })
-	router.GET("/constrain", func(c *gin.Context) { c.File("./static/constrain.html") })
-	router.GET("/localpeer", func(c *gin.Context) { c.File("./static/localpeer.html") })
+	group := router.Group("/webrtc")
+	group.GET("/:path", handleWebRTC)
 
+	router.GET("/", handleRoot)
 	router.Static("/js", "./static/js")
+
 	router.Run(":8000")
 }
--- WebRTC/static/js/localpeerConnection.js
+++ WebRTC/static/js/localpeerConnection.js
@@ -15,10 +15,12 @@ hangupButton.disabled = true;
 startButton.onclick = start;
 callButton.onclick = call;
 hangupButton.onclick = hangup;
+
 // Utility function for logging information to the JavaScript console
 function log(text) {
     console.log("At time: " + (performance.now() / 1000).toFixed(3) + " --> " + text);
 }
+
 // Callback in case of success of the getUserMedia() call
 function successCallback(stream) {
     log("Received local stream");
@@ -28,6 +30,7 @@ function successCallback(stream) {
     // We can now enable the Call button
     callButton.disabled = false;
 }
+
 // Function associated with clicking on the Start button
 // This is the event triggering all other actions
 function start() {
@@ -43,6 +46,7 @@ function start() {
             log("navigator.getUserMedia error: ", error);
         });
 }
+
 // Function associated with clicking on the Call button
 // This is enabled upon successful completion of the Start button handler
 function call() {
@@ -73,18 +77,18 @@ function call() {
         RTCIceCandidate = mozRTCIceCandidate;
     }
 
-    log("RTCPeerConnection object: " + RTCPeerConnection);
+    log("[call] RTCPeerConnection object: " + RTCPeerConnection);
     // This is an optional configuration string, associated with
     // NAT traversal setup
     var servers = null;
     // Create the local PeerConnection object
     localPeerConnection = new RTCPeerConnection(servers);
-    log("Created local peer connection object localPeerConnection");
+    log("[call] Created local peer connection object localPeerConnection");
     // Add a handler associated with ICE protocol events
     localPeerConnection.onicecandidate = gotLocalIceCandidate;
     // Create the remote PeerConnection object
     remotePeerConnection = new RTCPeerConnection(servers);
-    log("Created remote peer connection object remotePeerConnection");
+    log("[call] Created remote peer connection object remotePeerConnection");
     // Add a handler associated with ICE protocol events...
 
     remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
@@ -94,9 +98,12 @@ function call() {
     // Add the local stream (as returned by getUserMedia())
     // to the local PeerConnection.
     localPeerConnection.addStream(localStream);
-    log("Added localStream to localPeerConnection");
+    log("[call] Added localStream to localPeerConnection");
     // We're all set! Create an Offer to be 'sent' to the callee as soon
-    // as the local SDP is ready.
+    // as the local SDP is ready. it asks the browser to properly examine
+    // the internal state of the PeerConnection and generate an appropriate
+    // RTCSessionDescription object, thus initiating the Offer/Answer-state
+    // machine.
     localPeerConnection.createOffer(gotLocalDescription, onSignalingError);
 }
 
@@ -108,7 +115,8 @@ function onSignalingError(error) {
 function gotLocalDescription(description) {
     // Add the local description to the local PeerConnection
     localPeerConnection.setLocalDescription(description);
-    log("Offer from localPeerConnection: \n" + description.sdp);
+    
+    log("[gotLocalDescription] Offer from localPeerConnection: \n" + description.sdp);
     // ...do the same with the 'pseudoremote' PeerConnection
     // Note: this is the part that will have to be changed if you want
     // the communicating peers to become remote
@@ -117,16 +125,18 @@ function gotLocalDescription(description) {
     // Create the Answer to the received Offer based on the 'local' description
     remotePeerConnection.createAnswer(gotRemoteDescription, onSignalingError);
 }
+
 // Handler to be called when the remote SDP becomes available
 function gotRemoteDescription(description) {
     // Set the remote description as the local description of the
     // remote PeerConnection.
     remotePeerConnection.setLocalDescription(description);
-    log("Answer from remotePeerConnection: \n" + description.sdp);
+    log("[gotRemoteDescription] Answer from remotePeerConnection: \n" + description.sdp);
     // Conversely, set the remote description as the remote description of the
     // local PeerConnection
     localPeerConnection.setRemoteDescription(description);
 }
+
 // Handler to be called when hanging up the call
 function hangup() {
     log("Ending call");
@@ -147,27 +157,26 @@ function gotRemoteStream(event) {
     // Associate the remote video element with the retrieved stream
 
     remoteVideo.srcObject = event.stream;
-    remoteVideo.play();
 
-    log("Received remote stream");
+    console.log("[gotRemoteStream] Received remote stream:", event);
 }
 // Handler to be called whenever a new local ICE candidate becomes available
 function gotLocalIceCandidate(event) {
-    console.log("getLocalIceCandiate:", event)
+    console.log("[getLocalIceCandiate]:", event)
 
     if (event.candidate) {
         // Add candidate to the remote PeerConnection
         remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-        log("Local ICE candidate: \n" + event.candidate.candidate);
+        log("[gotLocalIceCandidate] Local ICE candidate: \n" + event.candidate.candidate);
     }
 }
 // Handler to be called whenever a new remote ICE candidate becomes available
 function gotRemoteIceCandidate(event) {
-    console.log("getRemotelIceCandiate:", event)
+    console.log("[getRemotelIceCandiate]:", event)
 
     if (event.candidate) {
         // Add candidate to the local PeerConnection
         localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-        log("Remote ICE candidate: \n " + event.candidate.candidate);
+        log("[getRemotelIceCandiate] Remote ICE candidate: \n " + event.candidate.candidate);
     }
 }
